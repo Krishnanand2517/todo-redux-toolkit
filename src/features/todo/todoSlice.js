08fb12f4074ff46f20250f80eaed21d3913aff33
Krishnanand Yadav <krishnanand2517@gmail.com>
2023-10-28 11:51:20 +0530

    Create the 'updateTodo' reducer

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -14,19 +14,27 @@ const todoSlice = createSlice({
     initialState,
     reducers: {
         addTodo: (state, action) => {
-            const newTodo = {
+            const newTodoObject = {
                 id: nanoid(),
                 text: action.payload
             };
 
-            state.todos.push(newTodo);
+            state.todos.push(newTodoObject);
         },
         removeTodo: (state, action) => {
             const todoId = action.payload;
 
             state.todos = state.todos.filter((todo) => todo.id !== todoId);
         },
-        updateTodo: (state, action) => {}
+        updateTodo: (state, action) => {
+            const todoId = action.payload.id;
+            const updatedTodoObject = {
+                id: todoId,
+                text: action.payload.text
+            };
+
+            state.todos = state.todos.map((todo) => (todo.id === todoId) ? updatedTodoObject : todo);
+        }
     }
 });
 
