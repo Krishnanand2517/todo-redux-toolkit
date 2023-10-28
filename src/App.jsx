08d204d1024c7c468683cca4f93facd171d35727
Krishnanand Yadav <krishnanand2517@gmail.com>
2023-10-28 11:15:13 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+const App = () => {
+
+  return (
+    <div className="p-4 bg-slate-500">
+      <h1 className="text-3xl text-center text-orange-400">Radhe Radhe</h1>
+    </div>
+  );
+};
+
+export default App;
