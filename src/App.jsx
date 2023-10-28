d08cb9d3dbc609cd5c86f6a0116ca8d8afb116b1
Krishnanand Yadav <krishnanand2517@gmail.com>
2023-10-28 16:07:25 +0530

    Ready to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { TodoForm, TodoList } from './components';
 
 const App = () => {
-
   return (
     <div className="py-4 min-h-screen bg-slate-800 text-white">
       <div className="w-full max-w-2xl mx-auto">
