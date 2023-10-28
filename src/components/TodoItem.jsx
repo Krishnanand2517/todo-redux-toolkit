d1f7ad359ecc72cbdff9e4f9ac4cf90ee50eb6d4
Krishnanand Yadav <krishnanand2517@gmail.com>
2023-10-28 15:28:38 +0530

    Add the feature to update todos

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,33 +1,69 @@
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { removeTodo } from '../features/todo/todoSlice';
+import { removeTodo, updateTodo } from '../features/todo/todoSlice';
 
 const TodoItem = ({ todo }) => {
     const dispatch = useDispatch();
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedText, setEditedText] = useState(todo.text);
 
     const removeHandler = () => {
         dispatch(removeTodo(todo.id));
     };
 
+    const handleUpdateClick = () => {
+        setIsEditing(true);
+    };
+
+    const handleSave = () => {
+        setIsEditing(false);
+        dispatch(updateTodo({ id: todo.id, text: editedText }));
+    };
+
     return (
         <div className="bg-gray-900 rounded-lg p-4 my-4 flex justify-between items-center">
+            <input
+                type="text"
+                value={editedText}
+                onChange={(e) => setEditedText(e.target.value)}
+                readOnly={!isEditing}
+                className="bg-transparent w-2/3 max-w-md"
+            />
             <div>
-                {todo.text}
-            </div>
-            <div>
-                <button
-                    className="text-white bg-orange-400 border-0 py-1 px-4 focus:outline-none hover:bg-orange-500 rounded text-md mr-4"
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 26 26"
-                        strokeWidth={1.2}
-                        stroke="currentColor"
-                        className="w-6 h-6"
+                {isEditing ? (
+                    <button
+                        onClick={handleSave}
+                        className="text-white bg-green-400 border-0 py-1 px-4 focus:outline-none hover:bg-green-500 rounded text-md mr-4"
                     >
-                        <path d="M 20.09375 0.25 C 19.5 0.246094 18.917969 0.457031 18.46875 0.90625 L 17.46875 1.9375 L 24.0625 8.5625 L 25.0625 7.53125 C 25.964844 6.628906 25.972656 5.164063 25.0625 4.25 L 21.75 0.9375 C 21.292969 0.480469 20.6875 0.253906 20.09375 0.25 Z M 16.34375 2.84375 L 14.78125 4.34375 L 21.65625 11.21875 L 23.25 9.75 Z M 13.78125 5.4375 L 2.96875 16.15625 C 2.71875 16.285156 2.539063 16.511719 2.46875 16.78125 L 0.15625 24.625 C 0.0507813 24.96875 0.144531 25.347656 0.398438 25.601563 C 0.652344 25.855469 1.03125 25.949219 1.375 25.84375 L 9.21875 23.53125 C 9.582031 23.476563 9.882813 23.222656 10 22.875 L 20.65625 12.3125 L 19.1875 10.84375 L 8.25 21.8125 L 3.84375 23.09375 L 2.90625 22.15625 L 4.25 17.5625 L 15.09375 6.75 Z M 16.15625 7.84375 L 5.1875 18.84375 L 6.78125 19.1875 L 7 20.65625 L 18 9.6875 Z"></path>
-                    </svg>
-                </button>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                        >
+                            <path fillRule="evenodd" clipRule="evenodd" d="M18.1716 1C18.702 1 19.2107 1.21071 19.5858 1.58579L22.4142 4.41421C22.7893 4.78929 23 5.29799 23 5.82843V20C23 21.6569 21.6569 23 20 23H4C2.34315 23 1 21.6569 1 20V4C1 2.34315 2.34315 1 4 1H18.1716ZM4 3C3.44772 3 3 3.44772 3 4V20C3 20.5523 3.44772 21 4 21L5 21L5 15C5 13.3431 6.34315 12 8 12L16 12C17.6569 12 19 13.3431 19 15V21H20C20.5523 21 21 20.5523 21 20V6.82843C21 6.29799 20.7893 5.78929 20.4142 5.41421L18.5858 3.58579C18.2107 3.21071 17.702 3 17.1716 3H17V5C17 6.65685 15.6569 8 14 8H10C8.34315 8 7 6.65685 7 5V3H4ZM17 21V15C17 14.4477 16.5523 14 16 14L8 14C7.44772 14 7 14.4477 7 15L7 21L17 21ZM9 3H15V5C15 5.55228 14.5523 6 14 6H10C9.44772 6 9 5.55228 9 5V3Z" fill="#0F0F0F"></path>
+                        </svg>
+                    </button>
+                ) : (
+                    <button
+                        onClick={handleUpdateClick}
+                        className="text-white bg-orange-400 border-0 py-1 px-4 focus:outline-none hover:bg-orange-500 rounded text-md mr-4"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 26 26"
+                            strokeWidth={1.2}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                        >
+                            <path d="M 20.09375 0.25 C 19.5 0.246094 18.917969 0.457031 18.46875 0.90625 L 17.46875 1.9375 L 24.0625 8.5625 L 25.0625 7.53125 C 25.964844 6.628906 25.972656 5.164063 25.0625 4.25 L 21.75 0.9375 C 21.292969 0.480469 20.6875 0.253906 20.09375 0.25 Z M 16.34375 2.84375 L 14.78125 4.34375 L 21.65625 11.21875 L 23.25 9.75 Z M 13.78125 5.4375 L 2.96875 16.15625 C 2.71875 16.285156 2.539063 16.511719 2.46875 16.78125 L 0.15625 24.625 C 0.0507813 24.96875 0.144531 25.347656 0.398438 25.601563 C 0.652344 25.855469 1.03125 25.949219 1.375 25.84375 L 9.21875 23.53125 C 9.582031 23.476563 9.882813 23.222656 10 22.875 L 20.65625 12.3125 L 19.1875 10.84375 L 8.25 21.8125 L 3.84375 23.09375 L 2.90625 22.15625 L 4.25 17.5625 L 15.09375 6.75 Z M 16.15625 7.84375 L 5.1875 18.84375 L 6.78125 19.1875 L 7 20.65625 L 18 9.6875 Z"></path>
+                        </svg>
+                    </button >
+                )}
                 <button
                     onClick={removeHandler}
                     className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
@@ -47,8 +83,8 @@ const TodoItem = ({ todo }) => {
                         />
                     </svg>
                 </button>
-            </div>
-        </div>
+            </div >
+        </div >
     );
 };
 
