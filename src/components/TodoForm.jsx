4fee5ab9b0f593c917735c065f49e1a90a1ef06a
Krishnanand Yadav <krishnanand2517@gmail.com>
2023-10-28 14:50:39 +0530

    Add the feature to add todos

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,14 +1,29 @@
-
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../features/todo/todoSlice';
 
 const TodoForm = () => {
+    const dispatch = useDispatch();
+
+    const [todoText, setTodoText] = useState("");
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        dispatch(addTodo(todoText));
+    };
+
     return (
-        <form className="flex justify-center mt-12 mb-16">
+        <form onSubmit={submitHandler} className="flex justify-center mt-12 mb-16">
             <input
                 type="text"
                 className="w-full bg-gray-600 rounded-l-lg px-4"
+                value={todoText}
                 placeholder="write your task here..."
+                onChange={(e) => setTodoText(e.target.value)}
             />
             <button
+                type="submit"
                 className="font-bold rounded-r-lg bg-blue-400 px-6 py-1"
             >
                 Add Task
